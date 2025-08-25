ff9fa13ed30f958a8a11bc2dfadd2c9bc6a29dfe
Ren <b.b.r.12.11@gmail.com>
2025-08-26 00:18:16 +0900

    add padding

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -46,124 +46,127 @@ export default function NewPost() {
     };
 
     return (
-        <div className="max-w-lg mx-auto mt-10 flex flex-col gap-8">
-            <MapSelector selectedLocation={selectedLocation} />
-            <Card>
-                <CardHeader className="font-bold">New Post</CardHeader>
-                <CardContent>
-                    <form
-                        className="flex flex-col gap-4"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            const formData = new FormData(e.currentTarget);
+        <div className="space-y-6 container mx-auto px-4 py-8">
+            <div className="max-w-lg mx-auto mt-10 flex flex-col gap-8">
+                <MapSelector selectedLocation={selectedLocation} />
+                <Card>
+                    <CardHeader className="font-bold">New Post</CardHeader>
+                    <CardContent>
+                        <form
+                            className="flex flex-col gap-4"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                const formData = new FormData(e.currentTarget);
 
-                            if (imageUrl) formData.append("imageUrl", imageUrl);
-                            if (selectedLocation) {
-                                formData.set(
-                                    "address",
-                                    selectedLocation.address
-                                ); 
-                                formData.set(
-                                    "lat",
-                                    selectedLocation.lat.toString()
-                                );
-                                formData.set(
-                                    "lng",
-                                    selectedLocation.lng.toString()
-                                );
-                            }
-                            startTransition(() => {
-                                createPost(formData); 
-                            });
-                        }}
-                    >
-                        <div>
-                            <label
-                                htmlFor=""
-                                className="block font-medium text-gray-700 mb-1"
-                            >
-                                Place Name
-                                <span className="text-red-500">*</span>
-                            </label>
-                            <input
-                                type="text"
-                                name="name"
-                                placeholder="Abbey Road..."
-                                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                required
-                            />
-                        </div>
-                        <div>
-                            <label className="block font-medium text-gray-700 mb-1">
-                                Address
-                                <span className="text-red-500">*</span>
-                            </label>
-
-                            <AddressInput
-                                onSelectAddress={handleAddressSelect}
-                            />
-                        </div>
-                        <div>
-                            <label
-                                htmlFor=""
-                                className="block font-medium text-gray-700 mb-1"
-                            >
-                                Visited On
-                            </label>
-                            <input
-                                type="date"
-                                name="date"
-                                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            />
-                        </div>
-                        <div>
-                            <label
-                                htmlFor=""
-                                className="block font-medium text-gray-700 mb-1"
-                            >
-                                My Experience
-                            </label>
-                            <textarea
-                                name="description"
-                                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            />
-                        </div>
-                        <div>
-                            <label>Place Image</label>
-
-                            {imageUrl && (
-                                <Image
-                                    src={imageUrl}
-                                    alt="Image Preview"
-                                    className="w-full mb-4 object-cover rounded-xl"
-                                    width={300}
-                                    height={200}
-                                />
-                            )}
-                            <UploadButton
-                                endpoint="imageUploader"
-                                onClientUploadComplete={(res) => {
-                                    if (res && res[0].ufsUrl) {
-                                        setImageUrl(res[0].ufsUrl);
-                                    }
-                                }}
-                                onUploadError={(error: Error) => {
-                                    console.error("Upload error: ", error);
-                                }}
-                            />
-                        </div>
-
-                        <Button
-                            variant="customIndigo"
-                            type="submit"
-                            className="font-bold"
-                            disabled={isPending}
+                                if (imageUrl)
+                                    formData.append("imageUrl", imageUrl);
+                                if (selectedLocation) {
+                                    formData.set(
+                                        "address",
+                                        selectedLocation.address
+                                    );
+                                    formData.set(
+                                        "lat",
+                                        selectedLocation.lat.toString()
+                                    );
+                                    formData.set(
+                                        "lng",
+                                        selectedLocation.lng.toString()
+                                    );
+                                }
+                                startTransition(() => {
+                                    createPost(formData);
+                                });
+                            }}
                         >
-                            {isPending ? "Posting..." : "Post"}
-                        </Button>
-                    </form>
-                </CardContent>
-            </Card>
+                            <div>
+                                <label
+                                    htmlFor=""
+                                    className="block font-medium text-gray-700 mb-1"
+                                >
+                                    Place Name
+                                    <span className="text-red-500">*</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder="Abbey Road..."
+                                    className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label className="block font-medium text-gray-700 mb-1">
+                                    Address
+                                    <span className="text-red-500">*</span>
+                                </label>
+
+                                <AddressInput
+                                    onSelectAddress={handleAddressSelect}
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor=""
+                                    className="block font-medium text-gray-700 mb-1"
+                                >
+                                    Visited On
+                                </label>
+                                <input
+                                    type="date"
+                                    name="date"
+                                    className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor=""
+                                    className="block font-medium text-gray-700 mb-1"
+                                >
+                                    My Experience
+                                </label>
+                                <textarea
+                                    name="description"
+                                    className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                />
+                            </div>
+                            <div>
+                                <label>Place Image</label>
+
+                                {imageUrl && (
+                                    <Image
+                                        src={imageUrl}
+                                        alt="Image Preview"
+                                        className="w-full mb-4 object-cover rounded-xl"
+                                        width={300}
+                                        height={200}
+                                    />
+                                )}
+                                <UploadButton
+                                    endpoint="imageUploader"
+                                    onClientUploadComplete={(res) => {
+                                        if (res && res[0].ufsUrl) {
+                                            setImageUrl(res[0].ufsUrl);
+                                        }
+                                    }}
+                                    onUploadError={(error: Error) => {
+                                        console.error("Upload error: ", error);
+                                    }}
+                                />
+                            </div>
+
+                            <Button
+                                variant="customIndigo"
+                                type="submit"
+                                className="font-bold"
+                                disabled={isPending}
+                            >
+                                {isPending ? "Posting..." : "Post"}
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     );
 }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -34,89 +34,100 @@ export default async function PostsPage() {
     }
     return (
         <div className="space-y-6 container mx-auto px-4 py-8">
-            <div className="flex items-center justify-between">
-                <h1 className="text-3xl font-bold tracking-tight">
-                    Posts List
-                </h1>
-                <Link href={"/posts/new"}>
-                    <Button variant="customIndigo">
-                        <Plus />
-                        Add Post
-                    </Button>
-                </Link>
-            </div>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Welcome back, {session.user?.name}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p>
-                        {posts.length === 0
-                            ? "Let's visit a Beatles spot and create your first post!"
-                            : `Total posts: ${posts.length}`}
-                    </p>
-                </CardContent>
-            </Card>
-            <div>
-                <h2 className="text-xl font-semibold mb-4">All Your Posts</h2>
-                {posts.length === 0 ? (
-                    <Card>
-                        <CardContent className="flex flex-col items-center justify-center py-8">
-                            <h3 className="text-xl font-medium mb-2">
-                                No posts yet.
-                            </h3>
-                            <p className="text-center mb-4 max-w-md">
-                                Create your first post.
-                            </p>
-                            <Link href={"/posts/new"}>
-                                <Button variant="customIndigo">New Post</Button>
-                            </Link>
-                        </CardContent>
-                    </Card>
-                ) : (
-                    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                        {sortedPosts.slice(0, 6).map((post, key) => (
-                            <Link key={key} href={`/posts/${post.id}?from=posts`}>
-                                <Card className="h-full hover:shadow-md transition">
-                                    <CardHeader className="line-clamp-1 font-semibold">
-                                        {post.title}
-                                    </CardHeader>
-                                    <CardContent>
-                                        <p className="text-sm text-gray-500 line-clamp-3 mb-2">
-                                            {post.location.address}
-                                        </p>
-                                        <p className="text-sm text-gray-500 line-clamp-1">
-                                            {`Date: ${
-                                                post.date
-                                                    ? new Date(
-                                                          post.date
-                                                      ).toLocaleDateString()
-                                                    : new Date(
-                                                          post.createdAt
-                                                      ).toLocaleDateString()
-                                            }`}
-                                        </p>
-                                    </CardContent>
-                                </Card>
-                            </Link>
-                        ))}
-                    </div>
-                )}
-            </div>
-            <div>
-                <h2 className="text-xl font-semibold mb-4">Map</h2>
-                <BaseMap
-                    center={
-                        posts.length > 0
-                            ? {
-                                  lat: posts[0].location.lat,
-                                  lng: posts[0].location.lng,
-                              }
-                            : { lat: 53.405471, lng: -2.979881 }
-                    }
-                    zoom={12}
-                    posts={posts}
-                />
+            <div className="space-y-6 container mx-auto px-4 py-8">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-bold tracking-tight">
+                        Posts List
+                    </h1>
+                    <Link href={"/posts/new"}>
+                        <Button variant="customIndigo">
+                            <Plus />
+                            Add Post
+                        </Button>
+                    </Link>
+                </div>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>
+                            Welcome back, {session.user?.name}
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <p>
+                            {posts.length === 0
+                                ? "Let's visit a Beatles spot and create your first post!"
+                                : `Total posts: ${posts.length}`}
+                        </p>
+                    </CardContent>
+                </Card>
+                <div>
+                    <h2 className="text-xl font-semibold mb-4">
+                        All Your Posts
+                    </h2>
+                    {posts.length === 0 ? (
+                        <Card>
+                            <CardContent className="flex flex-col items-center justify-center py-8">
+                                <h3 className="text-xl font-medium mb-2">
+                                    No posts yet.
+                                </h3>
+                                <p className="text-center mb-4 max-w-md">
+                                    Create your first post.
+                                </p>
+                                <Link href={"/posts/new"}>
+                                    <Button variant="customIndigo">
+                                        New Post
+                                    </Button>
+                                </Link>
+                            </CardContent>
+                        </Card>
+                    ) : (
+                        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                            {sortedPosts.slice(0, 6).map((post, key) => (
+                                <Link
+                                    key={key}
+                                    href={`/posts/${post.id}?from=posts`}
+                                >
+                                    <Card className="h-full hover:shadow-md transition">
+                                        <CardHeader className="line-clamp-1 font-semibold">
+                                            {post.title}
+                                        </CardHeader>
+                                        <CardContent>
+                                            <p className="text-sm text-gray-500 line-clamp-3 mb-2">
+                                                {post.location.address}
+                                            </p>
+                                            <p className="text-sm text-gray-500 line-clamp-1">
+                                                {`Date: ${
+                                                    post.date
+                                                        ? new Date(
+                                                              post.date
+                                                          ).toLocaleDateString()
+                                                        : new Date(
+                                                              post.createdAt
+                                                          ).toLocaleDateString()
+                                                }`}
+                                            </p>
+                                        </CardContent>
+                                    </Card>
+                                </Link>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <div>
+                    <h2 className="text-xl font-semibold mb-4">Map</h2>
+                    <BaseMap
+                        center={
+                            posts.length > 0
+                                ? {
+                                      lat: posts[0].location.lat,
+                                      lng: posts[0].location.lng,
+                                  }
+                                : { lat: 53.405471, lng: -2.979881 }
+                        }
+                        zoom={12}
+                        posts={posts}
+                    />
+                </div>
             </div>
         </div>
     );
